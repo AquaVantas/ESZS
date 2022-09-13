f66a375eea9da3fb5077dec25ae75aca2dc117ac
DESKTOP-2GC1C0L\aqua <aleksandra.lali@gmail.com>
2022-09-13 21:06:42 +0200

    first commit

---
+++ cpanel.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="Style/Master.css">
+	</head>
+	<body>
+		<div class="cpanel-navigation-wrapper">
+			<div class="right-side">T
+				<div class="logo-wrapper">
+					<div class="">
+					</div>
+				</div>
+				<div class="navigation-links">
+					<a href="?tab=webpage_editor">Spletna stran</a>
+					<a href="?tab=news_editor">Novice</a>
+					<a href="?tab=tournaments">Tekmovanja</a>
+				</div>
+			</div>
+			<div class="left-side">
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
+</html>
