24d00c1afb5c7b1108cd2c230e05a0b1a98eb780
DESKTOP-2GC1C0L\aqua <aleksandra.lali@gmail.com>
2022-09-13 23:00:32 +0200

    CPanel navigation styling, bootstrap, added fonts

--- cpanel.php
+++ cpanel.php
@@ -1,17 +1,23 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
+<?php
+	session_start();
+	$cpanel_tab = $_GET['tab'];
+?>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<link rel="stylesheet" href="Style/Master.css">
 	</head>
 	<body>
 		<div class="cpanel-navigation-wrapper">
-			<div class="right-side">T
+			<div class="right-side">
 				<div class="logo-wrapper">
 					<div class="">
+						EŠZS CPanel
 					</div>
 				</div>
 				<div class="navigation-links">
-					<a href="?tab=webpage_editor">Spletna stran</a>
+					<a href="?tab=webpage_editor" class="nav-link <?php if($cpanel_tab == "webpage_editor"){echo "active"; } ?>">Spletna stran</a>
 					<a href="?tab=news_editor">Novice</a>
 					<a href="?tab=tournaments">Tekmovanja</a>
 				</div>
