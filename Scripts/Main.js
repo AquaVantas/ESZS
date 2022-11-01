1eec94b6e5ed322098a42f830aec79791747161f
DESKTOP-2GC1C0L\aqua <aleksandra.lali@gmail.com>
2022-11-01 08:58:22 +0100

    Don't ask me, IDK, I just need everything here ,LP

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -7,3 +7,30 @@ function deleteUser(command) {
     console.log(command);
     $(".delete-user-modal .modal-footer .delete-button").attr("href", command);
 }
+
+function submitPageChanges(page_id, lang_id) {
+    let pageDetailsFormData = new FormData();
+    pageDetailsFormData.append("page_title", document.getElementById("page_title").value);
+    pageDetailsFormData.append("meta_name", document.getElementById("meta_name").value);
+    pageDetailsFormData.append("meta_description", document.getElementById("meta_description").value);
+    pageDetailsFormData.append("meta_keyword", document.getElementById("meta_keyword").value);
+
+    let pageDetailsFormDataXHR = new XMLHttpRequest();
+    pageDetailsFormDataXHR.open("POST", "Controllers/Website/Page/website_edit_page_details.php?page_id=" + page_id + "&lang_id=" + lang_id);
+    pageDetailsFormDataXHR.send(pageDetailsFormData);
+
+    let pageSectionBlockFormData = new FormData();
+
+    $(".accordion-item").each(function (index) {
+        if ($(".accordion-body", this).attr("variant-id") == 1) {
+            pageSectionBlockFormData.append("section-name", $(".accordion-body #section-name", this).val());
+            pageSectionBlockFormData.append("section-class", $(".accordion-body #section-class", this).val());
+            pageSectionBlockFormData.append("section-header", $(".accordion-body #section-header", this).val());
+            pageSectionBlockFormData.append("section-subheader", $(".accordion-body #section-subheader", this).val());
+        }
+        else if ($(".accordion-body", this).attr("variant-id") == 2) {
+            console.log("que");
+        }
+    });
+
+}
--- Views/CPanel/WebpageEditor/Page/tab_website_page_details_edit.php
+++ Views/CPanel/WebpageEditor/Page/tab_website_page_details_edit.php
@@ -21,9 +21,9 @@
 							</div>	
 						</div>
 					<?php } ?>
-						<div class="col-12 submit-field">
+						<!--<div class="col-12 submit-field">
 							<input class="btn btn-primary submit" type="submit" value="Posodobi stran">
-						</div>								
+						</div>-->						
 				</form>
 				<div class="col-12">
 					<div class="edit-cloud">
@@ -53,20 +53,40 @@
 												</button>
 											</h2>
 											<div id="collapse<?= $counter ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $counter ?>" data-bs-parent="#accordionExample">
-												<div class="accordion-body">
+												<div class="accordion-body" variant-id="<?= $section['WS_variant_id'] ?>" section-id="<?= $section['WS_section_id'] ?>">
 													<?php if($section['WS_variant_id'] == 1) { ?>															
-														<div class="section-block-info">
-															<label for="section-name">Ime sekcije:</label><br>
-															<input type="text" id="section-name" name="section-name" value="<?= $section['WSB_section_name'] ?>">
-															<label for="section-class">Class sekcije:</label><br>
-															<input type="text" id="section-class" name="section-class" value="<?= $section['WSB_section_class'] ?>">
-															<label for="section-header">Naslov sekcije:</label><br>
-															<input type="text" id="section-header" name="section-header" value="<?= $section['WSB_block_header'] ?>">
-															<label for="section-subheader">Podnaslov sekcije:</label><br>
-															<input type="text" id="section-subheader" name="section-subheader" value="<?= $section['WSB_block_subheader'] ?>">
-															<label for="section-rich-text">Besedilo sekcije:</label><br>																			
-															<!-- rich text editor -->
-															<?php include "Views/CPanel/Universal/rich_text_editor.php" ?>	
+														<div class="section-block-info row">
+															<div class="col-2 label">																
+																<label for="section-name">Ime sekcije:</label><br>
+															</div>
+															<div class="col-10">
+																<input type="text" id="section-name" name="section-name" value="<?= $section['WSB_section_name'] ?>">
+															</div>
+															<div class="col-2 label">
+																<label for="section-class">Class sekcije:</label><br>
+															</div>
+															<div class="col-10">
+																<input type="text" id="section-class" name="section-class" value="<?= $section['WSB_section_class'] ?>">
+															</div>
+															<div class="col-2 label">
+																<label for="section-header">Naslov sekcije:</label><br>
+															</div>
+															<div class="col-10">
+																<input type="text" id="section-header" name="section-header" value="<?= $section['WSB_block_header'] ?>">
+															</div>
+															<div class="col-2 label">
+																<label for="section-subheader">Podnaslov sekcije:</label><br>
+															</div>
+															<div class="col-10">
+																<input type="text" id="section-subheader" name="section-subheader" value="<?= $section['WSB_block_subheader'] ?>">
+															</div>
+															<div class="col-2 label">
+																<label for="section-rich-text">Besedilo sekcije:</label><br>																			
+															</div>
+															<div class="col-10">
+																<!-- rich text editor -->
+																<?php include "Views/CPanel/Universal/rich_text_editor.php" ?>	
+															</div>
 														</div>
 													<?php } elseif($section['WS_variant_id'] == 2) { ?>															
 														<div class="section-gallery-icon"></div><?= $section['WSB_block_header'] ?>
@@ -85,6 +105,9 @@
 						</div>
 					</div>	
 				</div>
+			</div>
+			<div class="col-12 submit-field">
+				<input class="btn btn-primary submit" onclick="submitPageChanges(<?= $_GET['page_id'] ?>, <?= isset($_GET['lang_id']) ? $_GET['lang_id'] : 1 ?>)" type="submit" value="Posodobi stran">
 			</div>
 		</div>
 	</div>
